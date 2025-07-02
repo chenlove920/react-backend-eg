d5e05986c9db462f7a6618aee05c6c32eca0b1cd
yumo <chenyinxilove@gmail.com>
2025-07-02 15:41:04 +0800

    Revert "简易aixos封装"
    This reverts commit adb6ade6e0aa68c41d560cc84ef761fc2b7191d1.

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 import { ViteAliases } from 'vite-aliases'
 // https://vite.dev/config/
 export default defineConfig({
-  define: {
-     __API_URL__: 'http://geek.itheima.net/v1_0',
-  },
   plugins: [react(),ViteAliases({prefix:'@'})],
 })
